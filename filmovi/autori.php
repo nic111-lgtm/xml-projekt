69ac49e669c825bed4b963a03dff919e024beb57
nic111-lgtm <niko.skrobot11@gmail.com>
2025-06-08 23:11:43 +0200

    Add files via upload

---
+++ filmovi/autori.php
@@ -0,0 +1,81 @@
+<?php
+
+// ucitaj nekretnine.xml
+$xml = simplexml_load_file('podaci/autori.xml');
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Filmovi</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="autori.php">Autori</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+
+<div class="container">
+
+        <table class="table mt-5">
+            <thead>
+                <tr>
+                    <th>Ime</th>
+                    <th>Broj filmova</th>
+                    <th>Godine</th>
+                    <th>Broj oskara</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($xml->nekretnina as $n): ?>
+                    <tr>
+                        <td><?php echo $n->ime; ?></td>
+                        <td><?php echo $n->brojf; ?></td>
+                        <td><?php echo $n->godine; ?></td>
+                        <td><?php echo $n->brojo; ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+
+        <nav aria-label="Page navigation example">
+  <ul class="pagination justify-content-center">
+    <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item"><a class="page-link" href="#">Next</a></li>
+  </ul>
+</nav>
+
+
+    </div>
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  </body>
+</html>
